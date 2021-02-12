c30534f888f899fdae43974abb54e9d2e1c390b3
Danila Kutenin <kutdanila@yandex.ru>
2021-02-12 11:05:56 +0000

    Add resume homework

--- README.md
+++ README.md
@@ -34,7 +34,7 @@
 
 1. [Shell Scripting](./homework/shell_scripting)
 1. [GDB Debug](./homework/gdb_debug)
-1. Публикация своего резюме через Github Actions
+1. [Публикация своего резюме через Github Actions](./homework/resume)
 
 ## Ссылки
 
---
+++ homework/resume/choose_system.py
@@ -0,0 +1,21 @@
+import hashlib
+
+name = 'Кутенин Данила Михайлович'
+
+systems = [
+'debian:10.1',
+'debian:10.2',
+'debian:10.3',
+'debian:10.8',
+'ubuntu:20.04',
+'ubuntu:20.10',
+'ubuntu:18.04',
+'ubuntu:18.10',
+'archlinux:latest',
+'fedora:34',
+'fedora:33',
+'fedora:32',
+'fedora:31',
+]
+
+print(systems[int(hashlib.md5(name.encode('utf-8')).hexdigest(), 16) % len(systems)])
